d9fc1c6df9930671633dd5860504ecbdea90891b
Mark Hanrahan <markh@Marks-MacBook-Pro.local>
2021-06-08 19:40:11 +0100

    Fix to Header on Smaller Devices + Minor UI centering

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import NavBar from "../NavBar";
 import Title from "../Title";
 import "./style.css";
 
 export default function Header(props) {
   const title = "Mark Hanrahan";
+  const widthRef = useRef(null);
+  const [device, setDevice] = useState({});
+
+  const smallDevice = { flexGrow: 0 };
+  const largeDevice = { flexGrow: 1 };
+
+  useEffect(
+    () => {
+      //Achieves centering on smaller devices onload of the webpage
+      widthRef.current.offsetWidth > 500
+        ? changeDevice(largeDevice)
+        : changeDevice(smallDevice);
+    },
+    [widthRef.current],
+    largeDevice,
+    smallDevice
+  );
+
+  const changeDevice = (device) => setDevice(device);
 
   return (
     <div className="headerBanner">
-      <div className="headerContainer">
-        <div className="leftContainer">
+      <div className="headerContainer" ref={widthRef}>
+        <div className="leftContainer" style={device}>
           <div className="left">
             <Title title={title} size="h1"></Title>
           </div>
